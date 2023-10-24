de82cc9bd90b52efddade46474f1698b423f49d6
Nkemjika Obi <nkemjikaobi@gmail.com>
2023-10-23 23:01:54 -0400

    Patient Statistics

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import PatientStatistics from './components/PatientStatistics/PatientStatistics';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<PatientStatistics />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#f5f5f5',
+		paddingTop: 100,
+		paddingHorizontal: 40,
+	},
 });
---
+++ components/PatientStatistics/PatientStatistics.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const PatientStatistics = () => {
+	return (
+		<View style={styles.card_wrapper}>
+			<View style={styles.card}>
+				<Text style={styles.card_title}>Admitted Patients</Text>
+				<Text style={styles.card_value}>23 Patients</Text>
+			</View>
+			<View style={styles.card}>
+				<Text style={styles.card_title}>High Priority Patients</Text>
+				<Text style={styles.card_value}>0 Patients</Text>
+			</View>
+		</View>
+	);
+};
+
+export default PatientStatistics;
+
+const styles = StyleSheet.create({
+	card_wrapper: {
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+	},
+	card: {
+		borderColor: 'rgba(143, 143, 143, 0.12)',
+		borderWidth: 1,
+		padding: 16,
+		height: 80,
+		backgroundColor: '#fff',
+		borderRadius: 8,
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	card_title: {
+		fontSize: 12,
+		fontWeight: 'medium',
+		color: '#8F8F8F',
+		marginBottom: 8,
+	},
+	card_value: {
+		fontWeight: 'bold',
+		fontSize: 20,
+		color: '#060606',
+	},
+});
