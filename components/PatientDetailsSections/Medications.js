35e17138a91ca0e02ffa6113f6eca172b642c487
Nkemjika Obi <nkemjikaobi@gmail.com>
2023-10-26 21:19:12 -0400

    Medications for patients

--- components/PatientDetailsSections/Medications.js
+++ components/PatientDetailsSections/Medications.js
@@ -1,12 +1,86 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
 const Medications = () => {
 	return (
-		<View>
-			<Text>Medications</Text>
-		</View>
+		<ScrollView>
+			<View style={styles.wrapper}>
+				<View>
+					<Text style={styles.title}>Malaria Medications</Text>
+					<Text style={styles.body}>Prescribed by Dr Franklin Nwoke</Text>
+				</View>
+				<Text style={styles.body}>23 August 2023</Text>
+			</View>
+			<View style={styles.wrapper}>
+				<View>
+					<Text style={styles.title}>Malaria Medications</Text>
+					<Text style={styles.body}>Prescribed by Dr Franklin Nwoke</Text>
+				</View>
+				<Text style={styles.body}>23 August 2023</Text>
+			</View>
+			<View style={styles.wrapper}>
+				<View>
+					<Text style={styles.title}>Malaria Medications</Text>
+					<Text style={styles.body}>Prescribed by Dr Franklin Nwoke</Text>
+				</View>
+				<Text style={styles.body}>23 August 2023</Text>
+			</View>
+			<View style={styles.wrapper}>
+				<View>
+					<Text style={styles.title}>Malaria Medications</Text>
+					<Text style={styles.body}>Prescribed by Dr Franklin Nwoke</Text>
+				</View>
+				<Text style={styles.body}>23 August 2023</Text>
+			</View>
+			<View style={styles.wrapper}>
+				<View>
+					<Text style={styles.title}>Malaria Medications</Text>
+					<Text style={styles.body}>Prescribed by Dr Franklin Nwoke</Text>
+				</View>
+				<Text style={styles.body}>23 August 2023</Text>
+			</View>
+			<View style={styles.wrapper}>
+				<View>
+					<Text style={styles.title}>Malaria Medications</Text>
+					<Text style={styles.body}>Prescribed by Dr Franklin Nwoke</Text>
+				</View>
+				<Text style={styles.body}>23 August 2023</Text>
+			</View>
+			<View style={styles.wrapper}>
+				<View>
+					<Text style={styles.title}>Malaria Medications</Text>
+					<Text style={styles.body}>Prescribed by Dr Franklin Nwoke</Text>
+				</View>
+				<Text style={styles.body}>23 August 2023</Text>
+			</View>
+		</ScrollView>
 	);
 };
 
 export default Medications;
+
+const styles = StyleSheet.create({
+	wrapper: {
+		display: 'flex',
+		alignItems: 'center',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		marginBottom: 24,
+		borderColor: 'lightgray',
+		borderWidth: 1,
+		borderLeftWidth: 0,
+		borderRightWidth: 0,
+		borderTopWidth: 0,
+		paddingBottom: 20,
+	},
+	title: {
+		fontSize: 14,
+		color: '#232324',
+		fontWeight: 'bold',
+		marginBottom: 4,
+	},
+	body: {
+		fontSize: 12,
+		color: '#393E41',
+	},
+});
